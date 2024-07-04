cdb9c3ba8f91044ba892464e0055218a59c18088
Adam Burski <adam.burski@zeto.swidnica.pl>
2024-07-04 14:26:53 +0200

    Add parent to child communication -> home to register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
@@ -16,12 +15,7 @@ export class AppComponent implements OnInit{
 
   private accountService = inject(AccountService);
 
-  title = 'DatingApp';
-  http = inject(HttpClient);
-  users: any;
-
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -33,11 +27,5 @@ export class AppComponent implements OnInit{
   }
 
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next:  response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
+
 }
